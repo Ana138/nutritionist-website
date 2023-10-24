041d0352174eb690416f1d4ded5f4edb67057a9a
Ana Mendes <anajessica.mdo@gmail.com>
2023-10-23 22:21:25 -0300

    adc alteracoes

--- autenticacao/views.py
+++ autenticacao/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .utils import password_is_valid
+from django.shortcuts import redirect
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+        if request.method == "GET":
+                return render(request, 'cadastro.html')
+        elif request.method == "POST":
+                username = request.POST.get('usuario')
+                senha = request.POST.get('senha')
+                email = request.POST.get('email')
+                confirmar_senha = request.POST.get('confirmar_senha')
+
+                if not password_is_valid(request, senha, confirmar_senha):
+                        return redirect('/auth/cadastro')				
+
+                return HttpResponse(confirmar_senha)
 
 def logar(request):
-    pass
+        return HttpResponse('Vc está no login')
+
+        
